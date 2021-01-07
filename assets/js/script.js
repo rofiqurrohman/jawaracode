fffbc1a357d6e1627355cd904e75f5bf0b86d066
Rofiqur Rohman <rofiqur.rohman@students.amikom.ac.id>
2021-01-07 11:11:49 +0700

    Change all

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,35 @@ window.onscroll = function () {
     iconList.style.color = "white";
   }
 };
+
+// Animasi Muncul
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  if (wScroll > $('.conten').offset().top - 300) {
+    $('.judul').addClass('ada');
+    $('.conten').each(function (i) {
+      setTimeout(function () {
+        $('.conten').eq(i).addClass('ada');
+      }, 800 * (i + 1));
+    });
+  }
+
+  if (wScroll > $('.pilihan').offset().top - 450) {
+    $('.pilihan').addClass('pilihan-muncul');
+  }
+
+  if (wScroll > $('#home').offset().top - 0) {
+    $('.top').addClass('top-muncul');
+  }
+
+  if (wScroll > $('.paket').offset().top - 100) {
+    $('.judulbelajar').addClass('judulbelajar-muncul');
+    $('.paket').each(function (i) {
+      setTimeout(function () {
+        $('.paket').eq(i).addClass('paket-muncul');
+      }, 500 * (i + 1));
+    });
+  }
+
+
+});
